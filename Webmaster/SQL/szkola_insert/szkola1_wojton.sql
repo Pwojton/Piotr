ceaeec1f61bb29660c74eb014453a6c8a5444e52
nazwa_użytkownika <email_uytkownika>
2018-05-18 13:31:43 +0200

    sql

---
+++ Webmaster/SQL/szkola_insert/szkola1_wojton.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS tbUczniowie;
+CREATE TABLE tbUczniowie (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	imie TEXT,
+	nazwisko TEXT,
+	plec INTEGER,
+	idKlasa INTEGER NOT NULL REFERENCES tbklasy(id),
+	egzHum NUMERIC NOT NULL DEFAULT 0,
+	egzMat NUMERIC NOT NULL DEFAULT 0,
+	egzJez NUMERIC NOT NULL DEFAULT 0
+);
+
+DROP TABLE IF EXISTS tbKlasy;
+CREATE TABLE tbKlasy (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+	klasa TEXT,
+	rokNaboru INTEGER,
+	rokMatury INTEGER
+);
+
+DROP TABLE IF EXISTS tbPrzedmioty;
+CREATE TABLE 	tbPrzedmioty (
+	id INTEGER  PRIMARY KEY AUTOINCREMENT,
+	przedmiot TEXT,
+	nazwiskoNaucz TEXT,
+	imiNaucz TEXT,
+	plecNaucz INTEGER
+);
+
+DROP TABLE IF EXISTS tbOceny;
+CREATE TABLE tbOceny (
+	id INTEGER  PRIMARY KEY AUTOINCREMENT,
+	ocena NUMERIC,
+	datad DATE,
+	idUczen INTEGER NOT NULL REFERENCES tbUczniowie(id),
+	idPrzedmiot INTEGER NOT NULL REFERENCES tbPrzedmioty(id)
+);
+
+INSERT INTO tbKlasy 	VALUES 	(NULL, '1A', 2015, 2018);
+INSERT INTO tbKlasy 	VALUES 	(NULL, '1B', 2015, 2018);
+
+INSERT INTO tbUczniowie VALUES	(NULL,'Adam', 'Kowalski', 1, 2, 80.5, 50,90 );
+INSERT INTO tbUczniowie VALUES	(NULL,'Adam', 'Kowalski', 1, 2, 80.5, 50,90 );
+INSERT INTO tbUczniowie VALUES	(NULL,'Ilona', 'Nowak', 1, 2, 80.5, 50,90 );
