11972113783849a30d96d486a79d435790500919
pwojton <piotr.wojton.01@gmail.com>
2018-10-01 21:55:23 +0200

    dodanie pracy domowej z pythona

--- Webmaster/python/petle-cw5.py
+++ Webmaster/python/petle-cw5.py
@@ -1,33 +1,65 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  cw2.py
-#
-#DANE WEJŚCIOWE:
-# liczby dodatnie podawane przez użytkownika
-#DANE WYJŚCIOWE:
-#suma liczb podanych przez użytkownika 
-#program musi zapewnić poprawnośc danych
-#program kończy działanie po wprowadzeniu wartości 0
-#
+#  pentle_cw_2.py
+
+
 
 
 
 def main(args):
     
     suma = 0
-    liczba = -1 
+    liczba = 1
     
     while liczba != 0:
-        liczba  = int(input("Podaj liczbę: "))
-        while liczba < 0:  # pętla zaporowa - pętla która wymusza wprowadzemie poprawnych danych
-            liczba  = int(input("Błedne dane! Podaj liczbę: "))
+        liczba = int(input("Podaj liczbę: "))
+        while liczba < 0:  # pętla zaporowa
+            liczba = int(input("Błędne dane! Wprowadzane liczby powinny być dodatnie. Podaj liczbę jeszcze raz: "))
         suma += liczba
-        
-    print("Suma: ", suma)
-    
+    print("suma: ", suma)
     return 0
+    
+def main2(args):
+    
+    suma = 0
+    liczba = 1
+    
+    while liczba != 0:
+        liczba = int(input("Podaj liczbę: "))
+        if liczba < 0:
+            print("Błędne dane! ", end='')
+        else:
+            suma += liczba
+    print("suma: ", suma)
+    return 0
+
+def main3(args):
+	
+	nazwa = ['','styczeń','luty','marzec','kwiecień','maj','czerwiec','lipiec','sierpień','wrzesień','paździenik','listopad','grudzień']
+	
+	a = 3
+	
+	while a > 0:
+		a = a - 1
+		miesiac = int(input("podaj numer miesiąca: "))
+		if miesiac > 12 or miesiac < 1:
+			print("Błędne dane! Wpisz raz: ")
+		else:
+			print (nazwa[miesiac])
+	return 0
+
+def main4(args):
+	suma = 0
+	liczby = []
+	a = int(input("Podaj liczbę: "))
+	
+	liczby = list(map(int, str(a)))
+
+	print (sum(liczby))
+	return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main4(sys.argv))
