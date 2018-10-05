32963e8c1f151942e367fb994abf8bc6d6990622
pwojton <piotr.wojton.01@gmail.com>
2018-10-05 14:19:34 +0200

    rysowanie figur - python ćwiczenia

--- Webmaster/python/figury.py
+++ Webmaster/python/figury.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 #  figury.py
@@ -11,26 +11,40 @@ def prostokat1(a, b):
         print()
  
  
-def prostokat2(a, b, c):
+def prostokat2(a, b):
+    """ Drukowanie pustego prostokąta """
     for i in range(a):
-        if i == 0 and i == b:
-            print("#", end='')
         for j in range(b):
-            if j == 0 or j == b -1:
+            if j == 0 or j == b -1 or i == 0 or i == a -1:
                 print("#", end='')
             else:
                 print(" ", end=' ')
         print()
 
+def choinka(h):
+    """ Drukowanie choinki """
+    for i in range(6):
+        for j in range(h + 1):
+            print("$", end='')
+        print()
+                        
     
+def trojkat(h, p):
+    """ Drukowanie trójkąta """
+    for i in range(6):
+        for j in range(7):
+            print("$", end='')
 
 def main(args):
-    a = int(input('Podaj szerokość prostokąta: '))
-    b = int(input('Podaj długość prostokąta: '))
-    c = int(input('Podaj wypełnienie prostokąta: '))
+    #a = int(input('Podaj DŁUGOŚĆ prostokąta: '))
+    #h = int(input('Podaj wysokość prostokąta: '))
+    #z = "z"
+    h = 6
+    p = 8
     
-    print(prostokat2(a, b, c))
+    choinka(h)
+
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+    sys.exit(trojkat(sys.argv))
