b3e1cabdb1b0e0bbef705c4f59a84ff3f4e78318
pwojton <piotr.wojton.01@gmail.com>
2018-10-09 22:18:05 +0200

    edytowanie figury.py

--- Webmaster/python/figury.py
+++ Webmaster/python/figury.py
@@ -1,50 +1,59 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 #  figury.py
-# Program drukuj wypełniony prostokąt o bokach podanych przez użytkownika 
- 
-def prostokat1(a, b):
-    for i in range(a):
-        for j in range(b):
-            print("#", end='')
-        print()
- 
- 
-def prostokat2(a, b):
-    """ Drukowanie pustego prostokąta """
-    for i in range(a):
-        for j in range(b):
-            if j == 0 or j == b -1 or i == 0 or i == a -1:
-                print("#", end='')
-            else:
-                print(" ", end=' ')
+#  
+def prostokat(a, b, c):
+    
+    for i in range (a):
+        for j in range (b):
+            print(c, end ='')
         print()
 
-def choinka(h):
-    """ Drukowanie choinki """
-    for i in range(6):
-        for j in range(h + 1):
-            print("$", end='')
+def prostokat_2(a, b, c):
+
+    for i in range (a):
+        for j in range (b):
+            if j == 0 or  j == b - 1 or i == 0 or i == a - 1:
+                print(c, end ='')
+            else:
+                print(" ", end = '')
         print()
-                        
+        
+def choinka(h, c):
     
-def trojkat(h, p):
-    """ Drukowanie trójkąta """
-    for i in range(6):
-        for j in range(7):
-            print("$", end='')
+    for i in range (h):
+        for j in range (i + 1):
+            print(c, end = '')
+        print ()
+
+def choinka_2(h, c):
+    
+    for i in range (h):
+        for j in range (h - i):
+            print(c, end = '')
+        print ()
+        
+def trojkat (h):
+    m = (h-1)*2
+    for i in range(h-1, -1, -1):
+        for j in range(m+1):
+            if j < i or j > m-i:
+                print(" ", end='')
+            else:
+                print("$", end='')
+        print()
 
 def main(args):
-    #a = int(input('Podaj DŁUGOŚĆ prostokąta: '))
-    #h = int(input('Podaj wysokość prostokąta: '))
-    #z = "z"
-    h = 6
-    p = 8
+#    a = int(input("Wysokość prostokąta:"))
+#    b = int(input("Długość prostokąta:"))
+    h = int(input("Wysokość trójkąta:"))
+#    c = input("podaj znkak wypenienia: ")
     
-    choinka(h)
-
+    print(trojkat(h))
+    
+    return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(trojkat(sys.argv))
+sys.exit(main(sys.argv))
