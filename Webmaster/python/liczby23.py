2edc9ca4f20846a97915d9f0cd64ac9d95bdec10
pwojton <piotr.wojton.01@gmail.com>
2018-10-10 20:28:11 +0200

    praca domowa liczby23.py

--- Webmaster/python/liczby23.py
+++ Webmaster/python/liczby23.py
@@ -19,10 +19,26 @@ def liczby2():
                 ile = ile + 1
     return ile
 
-
+def liczby3():
+    """ 
+    Funkcja drukuje wszystkie liczby 3-cyfrowe,
+    w których cyfry nie powtarzają się, Fukcja zwraca ich liczbę.
+    Wykluczone liczby: 111, 222, 333 itd.
+    """
+    
+    licznik = 0
+    
+    for i in range(1, 10):
+        for j in range(0, 10):
+            for k in range(0, 10):
+                if i != j or j != k or k != i:
+                    print("{}{}{} ".format(i, j,k), end='')
+                    licznik = licznik + 1
+    print()
+    return licznik
 
 def main(args):
-    print("\n\nLiczb 2-cyfrowych:", liczby2())
+    print("\n\nLiczb 3-cyfrowych:", liczby3())
     return 0
 
 if __name__ == '__main__':
