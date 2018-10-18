4330d4f8e9514e16c90ffc67f59818eb98264e37
pwojton <piotr.wojton.01@gmail.com>
2018-10-18 08:45:30 +0200

    pętlew cpp

---
+++ Webmaster/cpp/figury.cpp
@@ -0,0 +1,36 @@
+/*
+ * figury.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void prostokat(int a, int b, char znak) {
+    for (int i; i<a; i++){
+        for (int j; j<b; j++){
+            
+            }
+        }
+    
+    
+}
+
+int main(int argc, char **argv)
+{
+	int a, b; //deklaracja
+    a = b = 0; //inicjacja
+    // int a = 0; //defincja
+    cout << "Podaj bok1: ";
+    cin >> a;
+    cout << "Podaj bok2: ";
+    cin >> b;
+    
+    char znak;
+    cout << "Podaj znak: ";
+    cin >> znak;
+    
+    prostokat(a, b, znak)
+	return 0;
+}
+
