b277c6ab6ff16402953234743fce2829e044b52a
pwojton <piotr.wojton.01@gmail.com>
2018-11-14 10:35:40 +0100

    palindrom w cpp

---
+++ cpp/palindromy.cpp
@@ -0,0 +1,32 @@
+/*
+ * palindromy.cpp
+ */
+
+
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+bool palindrom(char w[], int r)
+{
+    bool pal = True;
+
+    for(int i = 0; i < (rozmiar /2); i++) {
+        if (wyraz[i] == wyraz[ilosc -(i+1)])
+        }
+
+    return pal;
+}
+
+int main(int argc, char **argv)
+{
+    int roz = 20;
+    char wyraz[20];
+    cout << "podaj wyraz: " ;
+    cin.getline(wyraz, roz);
+    cout << cin.gcount() << endl;
+    cout << strlen(wyraz) << endl;
+    
+	return 0;
+}
+
