74333c334f2360199d67d80f63fc571e113e4f4c
pwojton <piotr.wojton.01@gmail.com>
2018-11-15 08:45:40 +0100

    ciąg fibonacciego w cpp

---
+++ cpp/fiboncci.cpp
@@ -0,0 +1,40 @@
+/*
+ * fiboncci.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+long int fibonacci_it(int n)
+{
+    long int a = 0; // wyraz n-2
+    long int b = 1; // wyraz n-1
+
+    if(n == 0) return a;
+    else if (n == 1) return b;
+    int wynik = 0;
+    
+    for(int i = 2; i <= n; i++) {
+        wynik = a + b;
+        a = b;
+        b = wynik;
+    }
+    return wynik;
+}
+int main(int argc, char **argv)
+{
+    int n = 0;
+    int wynik = 0;
+    cout << "Podaj numer wyrazu ciągu: ";
+    cin >> n;
+    cout << "Ciąg Fibonacciego do wyrazu " << n << ":" << endl;
+
+    for (int i = 0; i < 0; i++){
+        cout << fibonacci_it(n) << endl;
+        wynik = i + 1;
+        }
+
+    return 0;
+}
+
