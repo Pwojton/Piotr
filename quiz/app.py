1f3ec577b92fbc2036785ad13090c86591a4a5ce
pwojton <piotr.wojton.01@gmail.com>
2019-01-07 08:45:46 +0100

    ÐZUPIETER

--- quiz/app.py
+++ quiz/app.py
@@ -2,19 +2,24 @@
 # -*- coding: utf-8 -*-
 #
 #  app.py
-#  
 from flask import g
 from modele import *
 from views import *
+
+app.config.update(dict(
+    SECRET_KEY='niesekretnyklucz',
+    TITLE='Aplikacja Quiz',
+))
 
 @app.before_request
 def before_request():
     g.db = baza
     g.db.connect()
 
-    
 @app.after_request
-def before_request(response):
+def after_request(response):
     g.db.close()
     return response
 
+if __name__ == '__main__':
+    app.run(debug=True)
