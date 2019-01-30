ec0a139abc322f3162e22678bf37c69c4dffff93
pwojton <piotr.wojton.01@gmail.com>
2019-01-30 10:35:28 +0100

    skonczonosc

---
+++ cpp/nieparzyste.cpp
@@ -0,0 +1,19 @@
+/*
+ * nieparzyste.cpp
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+    int n = 0;
+    int i;
+
+    cout << "Podaj liczbę: " << endl;
+    cin >> n;
+    
+	return 0;
+}
+
