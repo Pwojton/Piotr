d71f04f54c92bc1d8db22dade2a8d905ba5f1bda
pwojton <piotr.wojton.01@gmail.com>
2019-01-31 07:55:36 +0100

    systaem binarny

---
+++ cpp/dec2bin.cpp
@@ -0,0 +1,23 @@
+/*
+ * dec2bin.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void dec2bin (int l) {
+    int tab[];
+}
+
+int main(int argc, char **argv)
+{
+	int a;
+
+    cout << "Podaj liczbę: " << endl;
+    cin >> a;
+
+    
+	return 0;
+}
+
