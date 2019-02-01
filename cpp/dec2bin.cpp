b8a08fa36d90f260fbd6a48764d2a53a76d585b5
pwojton <piotr.wojton.01@gmail.com>
2019-02-01 14:20:57 +0100

    cos tam

--- cpp/dec2bin.cpp
+++ cpp/dec2bin.cpp
@@ -4,19 +4,57 @@
 
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-void dec2bin (int l) {
-    int tab[];
+int dec2any (int liczba, int podstawa, int tab[] ) {
+
+    int i =0;
+    do {
+        tab[i] = liczba % podstawa;
+        liczba = liczba / podstawa;
+        i++;
+    } while (liczba != 0);
+
+    return i-1;
 }
+
+
+void any2dec (int tab[]) {
+    int podstawa = 0;
+    int liczba10 = 0;
+    do {
+        cout << "Podstawa <2;9>: ";
+        cin >> podstawa;
+    } while (podstawa < 2 || podstawa > 9);
+    
+    int ile = 0;
+    cout << "Ile cyfr?"; cin >> ile;
+    for (int i = 0; i < ile; i++)
+        do {
+            cout << "Podaj cyfrę (0-)" << podstawa-1 <<"): ";
+            cin >> tab[i];
+        } while (tab[i]<0 || tab[i]>podstawa-1);
+    //konwersja system dziesiętny
+
+    for (int i =0; i < ile;i++) {
+        liczba10 +=pow (tab[i], ile -1-i);
+    }
+}
+
 
 int main(int argc, char **argv)
 {
-	int a;
-
-    cout << "Podaj liczbę: " << endl;
-    cin >> a;
-
+	int liczba, podstawa;
+    int tab[8];
+    cout << "Podaj liczbę i postawę systemu docelowego: ";
+    cin >> liczba >> podstawa;
+    int i = dec2any(liczba, podstawa, tab);
+    cout << "Wynik: ";
+    while (i >= 0) {
+        cout << tab[i];
+        i--;
+    }
     
 	return 0;
 }
