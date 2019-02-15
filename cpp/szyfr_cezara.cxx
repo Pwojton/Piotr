8e4d604ab360638e4eabd5d15749c2e25296e432
pwojton <piotr.wojton.01@gmail.com>
2019-02-15 14:19:06 +0100

    szyfr cezara

--- cpp/szyfr_cezara.cxx
+++ cpp/szyfr_cezara.cxx
@@ -5,30 +5,48 @@ using namespace std;
 
 #define MAKS 100
 
-void szyfruj(char tekst_jawny[], int klucz){
-    
-    int ilosc = strlen(tekst_jawny);
-    int kod;
+void szyfruj(char tekst[], int klucz){
+    klucz = klucz % 26;
+    int kod = 0;
+    int ilosc = strlen(tekst);
     
     for(int i = 0; i < ilosc; i++){
-        kod = (int)tb[i];
-            
+        kod = (int)tekst[i];
+        kod += klucz;
+        if (tekst[i] == ' ') {
+            kod -= klucz;
+            }
+        else if(kod > 122){
+            kod -= 26;
+        }
+        tekst[i] = char(kod);
+        cout << tekst[i];
     }
 
+}
+
+void deszyfruj(char tekst[], int klucz){
+    klucz = klucz % 26;
+    //int kod = 0;
+    int ilosc = strlen(tekst);
+
+    for(int i = 0; i < ilosc; i++){
+    }
+    
 }
 
 
 int main(int argc, char **argv)
 {
-    char tekst_jawny[MAKS];
+    char tekst[MAKS];
 	int klucz = 0;
     
     cout << "Podaj tekst: " << endl;
-    cin.getline(tekst_jawny, MAKS);
+    cin.getline(tekst, MAKS);
     
     cout << "Podaj klucz: " << endl;
     cin >> klucz;
     
-    szyfruj(tekst_jawny, klucz);
+    szyfruj(tekst, klucz);
 	return 0;
 }
